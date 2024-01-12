d80d7f429bfd39e612aac51c6f424db98e322f96
galib-1206 <bsse1206@iit.du.ac.bd>
2024-01-12 20:10:06 +0600

    Linking between pages

--- nextjs_demo/pages/news/index.js
+++ nextjs_demo/pages/news/index.js
@@ -1,7 +1,21 @@
 //our-domain.com/news..
+import { Fragment } from "react";
+import Link from 'next/link'
 
+//Fragment is used for list of items. 
+//Link is used insted of </a> for single page application
 function NewsPage() {
-    return <h1>The News Page</h1>
+    return(
+      <Fragment>
+        <h1>The News Page</h1>
+        <ul>
+          <li>
+            <Link href='/news/item1'>item1</Link>
+            </li>
+          <li>item2</li>
+        </ul>
+      </Fragment>
+    )
   }
   
   export default NewsPage;
