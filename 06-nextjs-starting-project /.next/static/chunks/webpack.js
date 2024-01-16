bddda409bf18c20361c25fb145e17192a88c26ce
galib-1206 <bsse1206@iit.du.ac.bd>
2024-01-16 16:13:52 +0600

    Pre rendering setup in Homepage

--- 06-nextjs-starting-project /.next/static/chunks/webpack.js
+++ 06-nextjs-starting-project /.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ae70f9007c053d0c9063";
+/******/ 	var hotCurrentHash = "2d46928557c62d50307e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- 06-nextjs-starting-project /pages/index.js
+++ 06-nextjs-starting-project /pages/index.js
@@ -1,5 +1,6 @@
 //our-domain.com
 //showing meetup lists
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 
@@ -24,11 +25,32 @@ const DUMMY_MEETUPS = [
 ];
 
 function HomePage() {
+  const [loadedMeetups, setLoadedMeetups]= useState([]);
+
+  useEffect(() => {
+    //send a http req & fetch data
+    setLoadedMeetups(DUMMY_MEETUPS)
+
+  }, []);
   return (
     
      
-      <MeetupList meetups={DUMMY_MEETUPS} />
+      <MeetupList meetups={loadedMeetups} />
   );
 }
 
 export default HomePage;
+/* Rendering:
+
+ At first Home component page rendering->loadedMeetups will be  an empty array
+ in that moment UseEffect will be executed->update state->component will be executed again
+ ->Rerendering list 
+
+so, there's is 2 render cycle -> create issue in search engine optimization(SEO)
+
+if we see in page source after 1st rendeing , items (are seen in the UI) will be found empty in HTML page
+which is fetching from server ! (becuase 2nd cycle is not occured yet)
+
+So, Pre rendering is a solution for this As it's not waiting for 2nd cycle 
+Also SEO optimization is quick , not loading!!!!!
+*/
